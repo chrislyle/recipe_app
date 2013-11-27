20554824f644760cdb4b17983cf22d48ec4c59fb
Chris Lyle <chrislyle@blueyonder.co.uk>
2013-11-27 21:53:05 +0000

    Initialize repository

---
+++ app/views/recipes/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @recipe, :title, :body, :created_at, :updated_at
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RecipeApp::Application.config.secret_key_base = '8c663760ec89d4aef39dc869afaece8e14df141c63e676a1b5f68c19ffdce550898202e26e2703a4a9d0f972d796c32e30cb41243f63a306ade42ca75e9e6949'
